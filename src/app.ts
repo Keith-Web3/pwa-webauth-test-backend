d33452461e438e93b55a87e287fc40926c511fe0
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2026-05-07 19:32:49 +0100

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import 'dotenv/config'
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(cors({ origin: process.env['ORIGIN'], credentials: true }))
 
 export default app
